57a3c3cadf8e57c16afb80bdc215be0c09776162
Alexandre Bulté <alexandre@bulte.net>
2016-12-23 09:32:26 +0100

    2 pics, pagination, normalize

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', init)
 
+var currentIndex
+var allMountains = []
+
 function fetchMountains () {
   return fetch('mountains.json')
     .then(function(response) {
@@ -13,8 +16,27 @@ function stopLoading () {
   document.querySelector('.loading').style.display = 'none'
 }
 
-function showMountain (mountain) {
-  document.querySelector('.image').src = 'pictures/' + mountain.picture
+function computePagination () {
+  var showPrevious = currentIndex < allMountains.length - 1
+  var showNext = currentIndex > 0
+  document.querySelector('.previous').style.visibility = showPrevious ? 'visible' : 'hidden'
+  document.querySelector('.next').style.visibility = showNext ? 'visible' : 'hidden'
+}
+
+function next () {
+  showMountain(currentIndex - 1)
+}
+
+function previous () {
+  showMountain(currentIndex + 1)
+}
+
+function showMountain (idx) {
+  var mountain = allMountains[idx]
+  currentIndex = idx
+  computePagination()
+  var image = document.querySelector('.image');
+  image.src = 'pictures/' + mountain.picture
   document.querySelector('.description').innerHTML = mountain.description
   if (mountain.copyright) {
     var hasLink = !!mountain.copyright_link
@@ -24,16 +46,19 @@ function showMountain (mountain) {
       el.target = '_blank'
     }
     el.innerHTML = mountain.copyright
+    document.querySelector('.copyright').innerHTML = ''
     document.querySelector('.copyright').appendChild(el)
   }
   document.querySelector('.description').innerHTML = mountain.description
+  image.alt = mountain.description
 }
 
 function init () {
   fetchMountains().then(function (mountains) {
     stopLoading()
+    allMountains = mountains.data
     if (mountains.data.length > 0) {
-      showMountain(mountains.data[0])
+      showMountain(0)
     }
   })
 }
